b36cf6f12919b0c16c180b014e11c90a0e6f6a03
Maksimens/Oriflame <Muskam.dan@gmail.com>
2025-10-23 14:14:50 +0300

    remove coments

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -4,5 +4,4 @@ users_bp = Blueprint('users', __name__,
                      url_prefix='/users',
                      template_folder='templates')
 
-# Імпортуємо маршрути, пов'язані з цим Blueprint
 from app.users import views
