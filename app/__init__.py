82254504352c937177c3f75cf215582393d512ef
Maksimens/Oriflame <Muskam.dan@gmail.com>
2025-10-28 15:34:55 +0200

    lab5 Solution

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,25 @@
 from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_pyfile("../config.py")
 # app.config['SECRET_KEY'] = 'a-super-secret-key-12345'
+
+# НАЛАШТУВАННЯ ЛОГЕРА (ДЛЯ ЛР5)
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/webapp.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Webapp startup')
 
 from . import views
 
